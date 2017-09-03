43f202933e90e035b9534877d336203c94a13271
vivianifederico <viviani.federico.maria@gmail.com>
2017-09-03 18:51:14 +0200

    first try with activemq

---
+++ Spring4MVCJmsActiveMQExample/src/main/java/com/websystique/springmvc/messaging/MessageSender.java
@@ -0,0 +1,31 @@
+package com.websystique.springmvc.messaging;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Component;
+
+import com.websystique.springmvc.model.Order;
+
+@Component
+public class MessageSender {
+
+	@Autowired
+	JmsTemplate jmsTemplate;
+
+	public void sendMessage(final Order order) {
+
+		jmsTemplate.send(new MessageCreator(){
+				@Override
+				public Message createMessage(Session session) throws JMSException{
+					return session.createObjectMessage(order);
+				}
+			});
+	}
+
+}
